33d4597c54edc656d7f532ca230c0608da2ed301
Jakob Fridesjö <tfy17jfo@cs.umu.se>
2022-02-10 23:53:51 +0100

    templates kinda work now

--- lab2/lab2-crud/src/main.rs
+++ lab2/lab2-crud/src/main.rs
@@ -1,9 +1,19 @@
 mod model;
 
 #[macro_use] extern crate rocket;
-use rocket::fs::FileServer;
+//use rocket::fs::FileServer;
+use rocket_dyn_templates::Template;
+use serde::Serialize;
 
+#[derive(Serialize)]
+struct IndexContext<'a> {
+    bar: &'a str,
+}
 
+#[derive(Serialize)]
+struct SnacksContext<'a> {
+    bar: &'a str,
+}
 
 #[post("/")]
 fn create() -> &'static str {
@@ -25,8 +35,23 @@ fn delete() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/snacks")]
+fn snacks() -> Template {
+    Template::render("snacks", SnacksContext {
+        bar: "Hello World!",
+    })
+}
+
+#[get("/")]
+fn index() -> Template {
+    Template::render("index", IndexContext {
+        bar: "Hello World!",
+    })
+}
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", FileServer::from("static"))
+    rocket::build()
+        .mount("/", routes![index,snacks])
+        .attach(Template::fairing())
 }
